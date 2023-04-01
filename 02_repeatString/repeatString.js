177d73c868bfb218de4e341cdf3778bf858cbd59
FlipFlop222 <bnsari789@gmail.com>
2023-04-01 12:25:53 +0300

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+    let string2 = string;
+    let returnString = "";
+    for (i = 0; i < num; i++) {
+        returnString = returnString + string2;
+    }
+    return returnString;
+    }
 };
 
 // Do not edit below this line
