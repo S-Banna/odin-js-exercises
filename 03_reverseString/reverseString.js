3db7c8558c0c162e36ecfbd6ff6afebac172f1f1
FlipFlop222 <bnsari789@gmail.com>
2023-04-01 12:48:13 +0300

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    let reverseArray = string.split("");
+    let reversed = "";
+    for (i = reverseArray.length - 1; i >=0; i--) {
+        reversed = reversed + reverseArray[i];
+    }
+    return reversed;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
