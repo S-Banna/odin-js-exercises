3d51f9b23ce6de8e4a0a94c6466a6b23838c2d38
FlipFlop222 <bnsari789@gmail.com>
2023-04-06 08:30:54 +0300

    Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numb) {
+  cels = (numb - 32) * (5/9);
+  return Math.round(cels * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numb) {
+  fahren = numb * 1.8 + 32;
+  return Math.round(fahren * 10) / 10;
 };
 
 // Do not edit below this line
