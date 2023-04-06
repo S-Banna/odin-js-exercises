86d69c9be6529fc1cf60af309eb5a210118e6b92
FlipFlop222 <bnsari789@gmail.com>
2023-04-06 08:03:25 +0300

    Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,15 @@
 const sumAll = function(num1, num2) {
     let i;
-    if (typeof(num1) != typeof(2) || typeof(num2) != typeof(2)) {
-        return "ERROR";
-    }
-    else {
-        if (num1 < 0 || num2 < 0) {
-            return "ERROR";
+    if (typeof(num1) != typeof(2) || typeof(num2) != typeof(2)) {return "ERROR";}
+    if (num1 < 0 || num2 < 0) {return "ERROR";}
+        if (num1 < num2) {
+            for (i = 0; num1 <= num2; num1++) {
+                i = i + num1;
+            }
         }
         else {
-            if (num1 < num2) {
-                for (i = 0; num1 <= num2; num1++) {
-                    i = i + num1;
-                }
-            }
-            else {
-                for (i = 0; num2 <= num1; num2++) {
-                    i = i + num2;
-                }
-            }
+        for (i = 0; num2 <= num1; num2++) {
+            i = i + num2;
         }
     }
     return i;
