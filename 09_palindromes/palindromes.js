a3a86a73ccf7a21d05c7d636dfda51ba590e7f89
FlipFlop222 <bnsari789@gmail.com>
2023-04-08 09:41:48 +0300

    Done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const punctuationless = string
+  .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+  .replace(/\s{2,}/g, " ");
+    const ready = punctuationless.toLowerCase().replace(/\s/g, "");
+    let compare = "";
+    for (let i = ready.length - 1; i >= 0; i--) {
+        compare += ready[i];
+    }
+    if (ready == compare) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
