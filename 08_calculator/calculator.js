f579f5999d27c9c30a6a8a53b5e9bbb8c0a97668
FlipFlop222 <bnsari789@gmail.com>
2023-04-08 08:59:33 +0300

    Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length == 0) {return 0}
+  return array.reduce((total, num) => {return total + num}); 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => {return total * num});
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {return 1};
+  let num2 = 1;
+  for (i = num; i > 0; i--) {
+    num2 = num2 * i;
+  }
+  return num2;
 };
 
 // Do not edit below this line
