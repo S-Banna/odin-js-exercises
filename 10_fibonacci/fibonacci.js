993558998070ceb43cf0d9ac35a10b0d797bebf5
FlipFlop222 <bnsari789@gmail.com>
2023-04-08 09:55:24 +0300

    Done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {return "OOPS"}
+    let fibo = [0, 1, 1];
+    for (i = 2; fibo[i] < 200000; i++) {
+        fibo.push(fibo[i] + fibo[i - 1]);
+    }
+    return fibo[num];
 };
 
 // Do not edit below this line
