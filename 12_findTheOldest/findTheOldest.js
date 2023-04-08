6428c3211ce95fe36339af67feccf30c39048761
FlipFlop222 <bnsari789@gmail.com>
2023-04-08 10:37:34 +0300

    Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let currentYear = new Date().getFullYear();
+    for (i = 0; i < array.length; i++) {
+        if (array[i].yearOfDeath == undefined) {
+            array[i].age = currentYear - array[i].yearOfBirth;
+        } else {
+            array[i].age = array[i].yearOfDeath - array[i].yearOfBirth;
+        }
+    }
+    let newArray = array.sort(function(a, b) {
+        if (a.age > b.age) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return array[0];
 };
 
 // Do not edit below this line
